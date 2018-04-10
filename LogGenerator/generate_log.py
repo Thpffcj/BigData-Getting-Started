55f712b4b2f22152942d341e8788258dda6a9319
Thpffcj <1441732331@qq.com>
2018-04-10 14:54:34 +0800

    来个产生数据的py

--- LogGenerator/generate_log.py
+++ LogGenerator/generate_log.py
@@ -5,64 +5,72 @@ import random
 import time
 
 url_path = [
-	"class/112.html",
-	"class/128.html",
-	"class/145.html",
-	"class/146.html",
-	"class/130.html",
-	"learn/821",
-	"course/list"
+    "class/112.html",
+    "class/128.html",
+    "class/145.html",
+    "class/146.html",
+    "class/130.html",
+    "learn/821",
+    "course/list"
 ]
 
 ip_slices = [132, 156, 124, 10, 29, 143, 187, 30, 46, 55, 63, 72, 98, 168]
 
 http_referers = [
-	"http://www.baidu.com/s?wd={query}",
-	"http://www.sogou.com/?web={query}",
-	"http://cn.bing.com/search?q={query}",
-	"https://search.yahoo.com/search?p={query}",
+    "http://www.baidu.com/s?wd={query}",
+    "http://www.sogou.com/?web={query}",
+    "http://cn.bing.com/search?q={query}",
+    "https://search.yahoo.com/search?p={query}",
 ]
 
 search_keyword = [
-	"Spark SQL实战",
-	"Hadoop基础",
-	"Storm实战",
-	"Spark Streaming实战",
-	"大数据面试"
+    "Spark SQL实战",
+    "Hadoop基础",
+    "Storm实战",
+    "Spark Streaming实战",
+    "大数据面试"
 ]
 
 status_code = ["200", "404", "500"]
 
+
 def sample_url():
-	return random.sample(url_path, 1)[0]
+    return random.sample(url_path, 1)[0]
 
 
 def sample_ip():
-	slice = random.sample(ip_slices, 4)
-	return ".".join([str(item) for item in slice])
+    slice = random.sample(ip_slices, 4)
+    return ".".join([str(item) for item in slice])
 
 
 def sample_referrer():
-	if random.uniform(0, 1) > 0.2:
-		return "-"
+    if random.uniform(0, 1) > 0.2:
+        return "-"
 
-	refer_str = random.sample(http_referers, 1)
-	query_str = random.sample(search_keyword, 1)
-	return refer_str[0].format(query=query_str[0])
+    refer_str = random.sample(http_referers, 1)
+    query_str = random.sample(search_keyword, 1)
+    return refer_str[0].format(query=query_str[0])
 
 
 def sample_status_code():
-	return random.sample(status_code, 1)[0]
+    return random.sample(status_code, 1)[0]
 
 
 def generate_log(count=10):
-	time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-	while count >= 1:
-		query_log = "{ip}\t{local_time}\t\"GET /{url} HTTP/1.1\"\t{status_code}\t{refer}".format(
-			ip=sample_ip(), local_time= time_str, url=sample_url(),  status_code=sample_status_code(), refer=sample_referrer())
-		print(query_log)
-		count = count - 1
+    time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    f = open("/home/thpffcj/data/project/logs/access.log", "a")
+
+    while count >= 1:
+        query_log = "{ip}\t{local_time}\t\"GET /{url} HTTP/1.1\"\t{status_code}\t{refer}".format(
+            ip=sample_ip(), local_time=time_str, url=sample_url(), status_code=sample_status_code(),
+            refer=sample_referrer())
+        print(query_log)
+
+        f.write(query_log + "\n")
+
+        count = count - 1
 
 
 if __name__ == '__main__':
-	generate_log()
+    generate_log()
---
+++ LogGenerator/message.py
@@ -0,0 +1,44 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'Thpffcj'
+
+import random
+import time
+
+infos = [
+    "116.397026,39.918058",
+    "116.410886,39.881949",
+    "116.272876,39.99243",
+    "116.544079,40.417555",
+    "116.225404,40.258186",
+    "116.38631,39.937209",
+    "116.399466,39.989743"
+]
+
+phones = [
+    "13888888888", "13877777777", "13866666666",
+    "13988888888", "13977777777", "13966666666",
+    "13788888888", "13777777777", "13766666666",
+    "13688888888", "13677777777", "13666666666",
+]
+
+def sample_phone():
+    return random.sample(phones, 1)[0]
+
+
+def sample_info():
+    return random.sample(infos, 1)[0]
+
+
+def generate_log(count = 3):
+    time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    f = open("/home/thpffcj/data/logs/access.log", "w+")
+    while count >= 1:
+        query_log = "{phone}\t{info}\t[{local_time}]".format(phone = sample_phone(),
+                                                             info = sample_info(), local_time = time_str)
+        # print(query_log)
+        f.write(query_log + "\t")
+        count = count - 1
+
+
+if __name__ == '__main__':
+    generate_log(10)
